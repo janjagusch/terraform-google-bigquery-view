71ffff68189515a93a376762cedb6fdcc41d59d4
jjagusch <jan.jagusch@gmail.com>
2020-07-06 13:09:05 +0200

    add template variable for query file

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "config_file" {
   type        = string
   description = "Path to the config.yaml"
 }
+
+variable "query_vars" {
+  type        = map(string)
+  description = "Values for placeholder variables in query"
+  default     = {}
+}
